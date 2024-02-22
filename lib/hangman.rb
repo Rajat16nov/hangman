9613f06b1809d8ef4dfe671a129b3abed3042890
rajat16nov <rajat16nov@gmail.com>
2024-02-22 16:31:53 +0530

    completes functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -16,25 +16,71 @@ class Game
   end
 
   def game_over?
-    @attempts > MAX_ATTEMPTS
+    @attempts >= MAX_ATTEMPTS
   end
 
-  def play
-    fetch_word_from_file
-    @guess = Array.new(@secret_word.length,"_")
-    display
-    puts "Enter any letter from a to z"
-    letter = gets.chomp
-    # until game_over?
-    # end
+  def is_alpha?(char)
+    char.match?(/[A-Za-z]/)
   end
+
+  def is_single?(char)
+    char.length == 1
+  end
+
+  def is_won?
+    @guess.join("") == @secret_word
+  end
+
+  def is_correct_word?(letter)
+    if letter == @secret_word
+      @guess = letter.split("")
+    end
+  end
+
+  def place_letters(guessed_letter)
+    @secret_word.split("").each_with_index do |letter, index|
+      if letter == guessed_letter
+        @guess[index] = guessed_letter
+      end
+    end
+  end
+
+
+  def play
+  fetch_word_from_file
+  @guess = Array.new(@secret_word.length,"_")
+  display
+  until game_over?
+    letter = ""
+    until is_alpha?(letter)
+      puts "Enter your best guess!"
+      letter = gets.chomp.downcase
+    end
+    @attempts += 1
+    if is_single?(letter)
+      place_letters(letter)
+    else
+      is_correct_word?(letter)
+    end
+    if is_won?
+      puts "Yeah, you guessed it right: #{@secret_word}"
+      break
+    end
+    display
+  end
+end
 
   def display
     puts @guess.join(" ")
+    attempts_remaining = MAX_ATTEMPTS - @attempts
+    if attempts_remaining == 0
+      puts "Game Over: The word was #{@secret_word}"
+    else
+      puts "Attempts remaining: #{attempts_remaining}"
+    end
   end
 
 end
-
 
 game = Game.new
 game.play
